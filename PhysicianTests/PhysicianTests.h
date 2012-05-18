d869ccf734f93c1b5f778ec838bba916fd554941
Raj Gokal <Raj@Rajs-MacBook-Pro.local>
2012-05-18 14:52:34 -0700

    Initial Commit

---
+++ PhysicianTests/PhysicianTests.h
@@ -0,0 +1,13 @@
+//
+//  PhysicianTests.h
+//  PhysicianTests
+//
+//  Created by Raj Gokal on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PhysicianTests : SenTestCase
+
+@end
---
+++ PhysicianTests/PhysicianTests.m
@@ -0,0 +1,32 @@
+//
+//  PhysicianTests.m
+//  PhysicianTests
+//
+//  Created by Raj Gokal on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PhysicianTests.h"
+
+@implementation PhysicianTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PhysicianTests");
+}
+
+@end
